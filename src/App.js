7fca6c4dad686b36d7a93ddca6db8ef429a685b9
kyotakata <kyotakata1@gmail.com>
2025-12-27 23:00:22 +0900

    課題1の実装(コンポーネント化前)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { posts } from "./data/post.js"
 
 function App() {
+  const [postDatas, setPosts] = useState(posts); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <a className="header-link" href="/">Blog</a>
+        <a className="header-link" href="/">お問い合わせ</a>
       </header>
+      <div class>
+        <div className="Home_container__cF\+Wd">
+          <ul>
+            {postDatas.map((post)=> (
+            <li className="Home_list__pwU2m">
+              <a className="Home_link__dG8iO" href="/">
+                <div className='Home_post__5TdqA'>
+                  <div>
+                    <div className='Home_postInfo__g-RmQ'>
+                      <div className='Home_postDate__tvUdI'>
+                        {new Date(post.createdAt).toISOString().slice(0,10)}</div>
+                      <div className='Home_postCategories__jjPkH'>
+                        {post.categories.map((cat)=>(
+                          <div className='Home_postCategory__MwsK3'>{cat}</div>
+                        ))}
+                      </div>
+                    </div>
+                    <p className='Home_postTitle__AlP6s'> {post.title}</p>
+                    <div className='Home_postBody__AnnY-'>{post.content}</div>
+                  </div>
+                </div>
+              </a>
+            </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
